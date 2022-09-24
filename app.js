bcf251dafe3c553ef03a4960905768480cf872bd
Philippe <phil-form@orange.fr>
2022-09-24 04:13:50 +0200

    Mise en place du serveur sur le port 8080.

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import path from "path";
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+export default app;
