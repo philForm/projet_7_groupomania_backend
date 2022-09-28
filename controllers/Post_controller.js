f356de00390d7c2626502a43df075beaa8f8dc35
Philippe <phil-form@orange.fr>
2022-09-28 07:09:33 +0200

    Creation des fonctions sendAllPosts et modifyPost

--- controllers/Post_controller.js
+++ controllers/Post_controller.js
@@ -1,7 +1,6 @@
 const { QueryTypes } = require("sequelize");
 const Db = require("../db/db.js")
 
-
 // Création d'un message
 const createPost = async (req, res, next) => {
     console.log(req.body);
@@ -19,5 +18,59 @@ const createPost = async (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
+// Envoie tous les messages
+const sendAllPosts = async (req, res, next) => {
+    const [posts] = await Db.query(`
+        SELECT post, post_picture, user_id, createdAt
+        FROM posts WHERE is_censored = 0
+        ORDER BY createdAt DESC;
+        `
+    );
+    console.log(posts);
+    res.send(posts);
 
-module.exports = { createPost };
+};
+
+const modifyPost = async (req, res, next) => {
+    // Récupération de l'id dans paramètres
+    const postId = JSON.parse(req.params.id);
+    // Sélection dans la BDD du post ayant le même id que la requête.
+    let fin = await Db.query(
+        `SELECT id FROM posts WHERE id = ?;`,
+        {
+            replacements: [postId],
+            type: QueryTypes.SELECT
+        }
+    );
+    
+    let result
+    for (let prop in fin) {
+        result = fin[prop].id
+        console.log(`result into : ${result}`)
+    };
+    console.log(`result out : ${result}`)
+
+    const { post, postPicture } = await req.body;
+    console.log(req.body.post);
+    
+    if (result != undefined) {
+
+        await Db.query(`
+            UPDATE posts
+            SET post = ?, post_picture = ?
+            WHERE id = ?;`,
+            {
+                replacements: [post, postPicture, postId],
+                type: QueryTypes.PUT
+            }
+        ).then(() => {
+            res.status(201).json({ message: "Message modifié !" });
+        })
+            .catch(error => res.status(500).json({ error }));
+    }
+        // res.send(500).json({message: "Post introuvable !"});
+
+}
+
+
+module.exports = { createPost, sendAllPosts, modifyPost };
--- routers/Posts_router.js
+++ routers/Posts_router.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const multer = require("multer");
 
-const { createPost } = require("../controllers/Post_controller.js")
+const { createPost, sendAllPosts, modifyPost } = require("../controllers/Post_controller.js")
 
 
 const postRouter = Router();
 
 postRouter.post("/", createPost);
+postRouter.get("/", sendAllPosts)
+postRouter.put("/:id", modifyPost);
 
 
 module.exports = postRouter;
