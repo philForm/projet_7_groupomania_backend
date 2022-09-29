4e2150f4ce0798f97d430fa2a085b4f97d6c503f
Philippe <phil-form@orange.fr>
2022-09-29 04:55:04 +0200

    Finalisation temporaire de Post_controller.js

--- controllers/Post_controller.js
+++ controllers/Post_controller.js
@@ -1,4 +1,5 @@
 const { QueryTypes } = require("sequelize");
+const fs = require("fs") 
 const Db = require("../db/db.js")
 
 // Création d'un message
@@ -31,28 +32,65 @@ const sendAllPosts = async (req, res, next) => {
 
 };
 
-const modifyPost = async (req, res, next) => {
-    // Récupération de l'id dans paramètres
+// Renvoie l'Id de la requête et l'Id de la BDD
+const idOfBd = async (req) => {
+    // Récupération de l'id dans les paramètres de la requête
     const postId = JSON.parse(req.params.id);
+    console.log(`postId into idOfBd : ${postId}`)
     // Sélection dans la BDD du post ayant le même id que la requête.
-    let fin = await Db.query(
+    let idIntoBdd = await Db.query(
         `SELECT id FROM posts WHERE id = ?;`,
         {
             replacements: [postId],
             type: QueryTypes.SELECT
         }
     );
-    
+
     let result
-    for (let prop in fin) {
-        result = fin[prop].id
+    for (let item in idIntoBdd) {
+        result = idIntoBdd[item].id
         console.log(`result into : ${result}`)
     };
-    console.log(`result out : ${result}`)
+    console.log(`result out : ${result}`);
+    return [result, postId];
+
+}
+
+/// Modifie un Post
+const modifyPost = async (req, res, next) => {
+    // ===========================================
+    // // Récupération de l'id dans les paramètres de la requête
+    // const postId = JSON.parse(req.params.id);
+    // // Sélection dans la BDD du post ayant le même id que la requête.
+    // let idIntoBdd = await Db.query(
+    //     `SELECT id FROM posts WHERE id = ?;`,
+    //     {
+    //         replacements: [postId],
+    //         type: QueryTypes.SELECT
+    //     }
+    // );
+
+    // let result
+    // for (let item in idIntoBdd) {
+    //     result = idIntoBdd[item].id
+    //     console.log(`result into : ${result}`)
+    // };
+    // console.log(`result out : ${result}`)
+    // ===========================================
+    
+    /// Tableau qui récupère les Ids renvoyés par idOfBd().
+    const tab = (await idOfBd(req)).map(el => el);
+    // console.log(`tab : ${tab}`);
+    /// Id de la requête
+    const postId = tab[1];
+    console.log(`postId : ${postId}`)
+    /// Id de la BDD
+    const result = tab[0];
 
     const { post, postPicture } = await req.body;
     console.log(req.body.post);
-    
+
+    /// Si dans la BDD un Id correspond à l'Id de la requête, le message est modifié
     if (result != undefined) {
 
         await Db.query(`
@@ -67,10 +105,41 @@ const modifyPost = async (req, res, next) => {
             res.status(201).json({ message: "Message modifié !" });
         })
             .catch(error => res.status(500).json({ error }));
-    }
-        // res.send(500).json({message: "Post introuvable !"});
+    } else
+        res.status(404).json({ message: "Post introuvable !" });
+}
+
+// Supprime un Post
+const deletePost = async (req, res, next) => {
+    
+    /// Tableau qui récupère les Ids renvoyés par idOfBd().
+    const tab = (await idOfBd(req)).map(el => el);
+    /// Id de la requête
+    const postId = tab[1];
+    /// Id de la BDD
+    const result = tab[0];
+    
+    /// Si dans la BDD un Id correspond à l'Id de la requête, le message est supprimé.
+    if (result != undefined) {
+
+        await Db.query(`
+            DELETE FROM posts
+            WHERE id = ?;`,
+            {
+                replacements: [postId],
+                type: QueryTypes.PUT
+            }
+        ).then(() => {
+            res.status(201).json({ message: "Message supprimé !" });
+        })
+            .catch(error => res.status(500).json({ error }));
+    } else
+        res.status(404).json({ message: "Post introuvable !" });
+    
+    
+
 
 }
 
 
-module.exports = { createPost, sendAllPosts, modifyPost };
+module.exports = { createPost, sendAllPosts, modifyPost, deletePost };
--- routers/Posts_router.js
+++ routers/Posts_router.js
@@ -1,14 +1,15 @@
 const { Router } = require("express");
 const multer = require("multer");
 
-const { createPost, sendAllPosts, modifyPost } = require("../controllers/Post_controller.js")
+const { createPost, sendAllPosts, modifyPost, deletePost } = require("../controllers/Post_controller.js");
 
 
 const postRouter = Router();
 
 postRouter.post("/", createPost);
-postRouter.get("/", sendAllPosts)
+postRouter.get("/", sendAllPosts);
 postRouter.put("/:id", modifyPost);
+postRouter.delete("/:id", deletePost);
 
 
 module.exports = postRouter;
