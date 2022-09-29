36f805d7141dba2cfb85bda7cb0ba152817051e3
Philippe <phil-form@orange.fr>
2022-09-29 04:59:14 +0200

    Suppression de code en doublon dans Post_controller.js

--- controllers/Post_controller.js
+++ controllers/Post_controller.js
@@ -1,5 +1,5 @@
 const { QueryTypes } = require("sequelize");
-const fs = require("fs") 
+const fs = require("fs")
 const Db = require("../db/db.js")
 
 // Création d'un message
@@ -37,6 +37,7 @@ const idOfBd = async (req) => {
     // Récupération de l'id dans les paramètres de la requête
     const postId = JSON.parse(req.params.id);
     console.log(`postId into idOfBd : ${postId}`)
+    
     // Sélection dans la BDD du post ayant le même id que la requête.
     let idIntoBdd = await Db.query(
         `SELECT id FROM posts WHERE id = ?;`,
@@ -58,26 +59,7 @@ const idOfBd = async (req) => {
 
 /// Modifie un Post
 const modifyPost = async (req, res, next) => {
-    // ===========================================
-    // // Récupération de l'id dans les paramètres de la requête
-    // const postId = JSON.parse(req.params.id);
-    // // Sélection dans la BDD du post ayant le même id que la requête.
-    // let idIntoBdd = await Db.query(
-    //     `SELECT id FROM posts WHERE id = ?;`,
-    //     {
-    //         replacements: [postId],
-    //         type: QueryTypes.SELECT
-    //     }
-    // );
 
-    // let result
-    // for (let item in idIntoBdd) {
-    //     result = idIntoBdd[item].id
-    //     console.log(`result into : ${result}`)
-    // };
-    // console.log(`result out : ${result}`)
-    // ===========================================
-    
     /// Tableau qui récupère les Ids renvoyés par idOfBd().
     const tab = (await idOfBd(req)).map(el => el);
     // console.log(`tab : ${tab}`);
@@ -111,14 +93,14 @@ const modifyPost = async (req, res, next) => {
 
 // Supprime un Post
 const deletePost = async (req, res, next) => {
-    
+
     /// Tableau qui récupère les Ids renvoyés par idOfBd().
     const tab = (await idOfBd(req)).map(el => el);
     /// Id de la requête
     const postId = tab[1];
     /// Id de la BDD
     const result = tab[0];
-    
+
     /// Si dans la BDD un Id correspond à l'Id de la requête, le message est supprimé.
     if (result != undefined) {
 
@@ -135,8 +117,8 @@ const deletePost = async (req, res, next) => {
             .catch(error => res.status(500).json({ error }));
     } else
         res.status(404).json({ message: "Post introuvable !" });
-    
-    
+
+
 
 
 }
